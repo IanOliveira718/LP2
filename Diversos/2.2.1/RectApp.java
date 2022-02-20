2b9cf76f675cf9ab12e1904bfb547e1587991684
Ian Oliveira <93954736+IanOliveira718@users.noreply.github.com>
2022-02-20 20:46:18 -0300

    Update RectApp.java

--- Diversos/2.2.1/RectApp.java
+++ Diversos/2.2.1/RectApp.java
@@ -24,9 +24,8 @@ class RectFrame extends JFrame {
         this.setTitle("Rects");
         this.setSize(350, 350);
         this.r1 = new Rect(50,50, 100,150,0,0,0,255,0,0);
-	  this.r2 = new Rect(170,50, 50,50,255,0,0,0,0,0);
-	  this.r3 = new Rect(250,50, 50,200,0,255,0,255,0,255);
-      
+	this.r2 = new Rect(170,50, 50,50,255,0,0,0,0,0);
+	this.r3 = new Rect(250,50, 50,200,0,255,0,255,0,255);  
     }
 
     public void paint (Graphics g) {
@@ -48,18 +47,16 @@ class Rect {
         this.y = y;
         this.w = w;
         this.h = h;
-	   this.r = r;
+	this.r = r;
         this.g = g;
         this.b = b;
-	  this.ir = ir;
+	this.ir = ir;
         this.ig = ig;
-        this.ib = ib;
-
-	  
+        this.ib = ib;  
     }
     void paint (Graphics g) {
 	  g.setColor(new Color(this.r,this.g,this.b));
-        g.drawRect(this.x,this.y, this.w,this.h);
+          g.drawRect(this.x,this.y, this.w,this.h);
 	  g.setColor(new Color(this.ir,this.ig,this.ib));
 	  g.fillRect(this.x,this.y, this.w,this.h);
     }
