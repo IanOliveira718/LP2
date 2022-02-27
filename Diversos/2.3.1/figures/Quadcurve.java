e42a8d0b2f2335099730bd2dda9e9b5fd1f4b191
Ian Oliveira <93954736+IanOliveira718@users.noreply.github.com>
2022-02-27 18:09:02 -0300

    Add files via upload

---
+++ Diversos/2.3.1/figures/Quadcurve.java
@@ -0,0 +1,29 @@
+package figures;
+
+import java.awt.*;
+import java.awt.geom.*;
+
+public class Quadcurve{
+
+	int x,y,cx,cy,x2, y2;
+
+	public Quadcurve(int x,int y,int cx,int cy,int x2,int y2){
+	this.x=x;
+	this.y=y;	
+	this.cx=cx; 
+	this.cy=cy; 
+	this.x2=x2; 
+	this.y2=y2;
+
+	}
+	public void print(){
+	  System.out.format("begin (%d,%d)/ control points (%d,%d)/ final (%d,%d)", this.x, this.y, this.cx, this.cy, this.x2, this.y2);
+	}
+	public void paint (Graphics g) {
+	   Graphics2D g2d = (Graphics2D) g;
+        QuadCurve2D q = new QuadCurve2D.Float();
+	  q.setCurve(this.x, this.y, this.cx, this.cy, this.x2, this.y2);
+	  g2d.draw(q);
+    }
+
+}
