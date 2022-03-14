4df474782949f0be4ab1ed2bf237b4904fb0fe65
Ian Oliveira <93954736+IanOliveira718@users.noreply.github.com>
2022-03-14 00:55:53 -0300

    Add files via upload

---
+++ Diversos/2.6.1/figures/Figure.java
@@ -0,0 +1,5 @@
+package figures;
+import java.awt.Graphics;
+public abstract class Figure{
+   public abstract void paint(Graphics g);
+}
---
+++ Diversos/2.6.1/figures/Rect.java
@@ -0,0 +1,34 @@
+package figures;
+
+import java.awt.*;
+
+public class Rect extends Figure  {
+    int x, y;
+    int w, h;
+    int r,g,b;
+    int ir,ig,ib;
+
+
+    public Rect (int x, int y, int w, int h,int r, int g, int b, int ir, int ig, int ib){
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+	   this.r = r;
+        this.g = g;
+        this.b = b;
+	  this.ir = ir;
+        this.ig = ig;
+        this.ib = ib;
+
+    }
+
+    public void paint (Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        g.setColor(new Color(this.r,this.g,this.b));
+        g.drawRect(this.x,this.y, this.w,this.h);
+	  g.setColor(new Color(this.ir,this.ig,this.ib));
+	  g.fillRect(this.x,this.y, this.w,this.h);
+
+    }
+}
