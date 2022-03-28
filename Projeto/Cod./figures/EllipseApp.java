be9f3cc38fce51b96fbb1ab78c609dbdf24ad4e8
Ian Oliveira <93954736+IanOliveira718@users.noreply.github.com>
2022-03-27 23:09:37 -0300

    Update EllipseApp.java

--- Projeto/Cod./figures/EllipseApp.java
+++ Projeto/Cod./figures/EllipseApp.java
@@ -31,4 +31,9 @@ public class Ellipse extends Figure{
 public void local(){
 	System.out.format("Local %d %d %d %d",this.x,this.y,this.w,this.h);
 	}
+	public void drag(int x,int y){
+		this.x=this.x+x;
+		this.y=this.y+y;
+	}
+
 }
