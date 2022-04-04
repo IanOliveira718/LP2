8099ac9fab7c792f5a10eb8d70ebd95b72b31660
Ian Oliveira <93954736+IanOliveira718@users.noreply.github.com>
2022-04-03 23:00:48 -0300

    Update Figure.java

--- Projeto/Cod./figures/Figure.java
+++ Projeto/Cod./figures/Figure.java
@@ -6,4 +6,9 @@ public abstract class Figure{
    public abstract void paint(Graphics g);
    public abstract void local();
    public abstract void drag(int x,int y);
+   public abstract void mdrag(int x, int y);
+   public abstract boolean VerP(int x,int y, Figure f);
+   public abstract void psize(int h, int w);
+   public abstract void changeC(int g, int r, int b);
+   public abstract void changeCi(int g, int r, int b);
 }
