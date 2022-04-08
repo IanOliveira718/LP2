15008bb51db0e978d0002e5d847bd73ec4976ee0
Ian Oliveira <93954736+IanOliveira718@users.noreply.github.com>
2022-04-08 11:41:46 -0300

    Update ListApp.java

--- Projeto/Cod./ListApp.java
+++ Projeto/Cod./ListApp.java
@@ -136,7 +136,7 @@ class ListFrame extends JFrame {
                                 save=null;
                                 fs.remove(i);
                                 repaint();
-				k=k-1
+				k=k-1;
                             }
                         }
 			
