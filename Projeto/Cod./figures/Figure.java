37bbf8de90f85a1ca614561fcf3ec07a6a10a8b1
Ian Oliveira <93954736+IanOliveira718@users.noreply.github.com>
2022-04-07 22:10:46 -0300

    Update Figure.java

--- Projeto/Cod./figures/Figure.java
+++ Projeto/Cod./figures/Figure.java
@@ -3,6 +3,7 @@ import java.awt.Graphics;
 public abstract class Figure{
    public int x, y,w,h;
    public int r,g,b;
+   public boolean foco; 
    public abstract void paint(Graphics g);
    public abstract void local();
    public abstract void drag(int x,int y);
