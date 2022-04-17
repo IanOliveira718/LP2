63273fc00fe8525eee3c1d9c328bae3ee53535b6
Ian Oliveira <93954736+IanOliveira718@users.noreply.github.com>
2022-04-17 17:09:02 -0300

    Update Figure.java

--- Projeto/Cod./figures/Figure.java
+++ Projeto/Cod./figures/Figure.java
@@ -1,14 +1,17 @@
 package figures;
-import java.awt.Graphics;
+
 public abstract class Figure{
    public int x, y,w,h;
    public int r,g,b;
    public boolean foco; 
-   public abstract void paint(Graphics g);
    public abstract void local();
    public abstract void drag(int x,int y);
    public abstract void mdrag(int x, int y);
-   public abstract boolean VerP(int x,int y, Figure f);
+   
+   public abstract boolean clicked(int x,int y, Figure f){
+   return(this.x<=x && x<=this.x+this.w && this.y<=y && y<=this.y+this.h);
+   }
+   
    public abstract void psize(int h, int w);
    public abstract void changeC(int g, int r, int b);
    public abstract void changeCi(int g, int r, int b);
