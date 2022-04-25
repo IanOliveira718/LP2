d56dfba2b312db5a116bc3e9ff67a0bbc617384f
Ian Oliveira <93954736+IanOliveira718@users.noreply.github.com>
2022-04-24 23:07:36 -0300

    Update Figure.java

--- Projeto/Cod./figures/Figure.java
+++ Projeto/Cod./figures/Figure.java
@@ -4,7 +4,7 @@ import ivisible.iVisible;
 
 public abstract class Figure{
    public int x, y,w,h;
-   public int r,g,b;
+   protected int r,g,b;
    public boolean foco; 
    public abstract void local();
    public abstract void drag(int x,int y);
