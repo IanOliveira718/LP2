5883d6ab85bec9279de0ace18bf5680ed4b10048
Ian Oliveira <93954736+IanOliveira718@users.noreply.github.com>
2022-05-08 18:56:37 -0300

    Update iVisible.java

--- Projeto/Cod./ivisible/iVisible.java
+++ Projeto/Cod./ivisible/iVisible.java
@@ -6,5 +6,9 @@ public interface iVisible{
 
     public boolean clicked(int x, int y);
     public void paint (Graphics g);
-
+    
+    public boolean clickdrag(int x, int y);
+    public void setf(boolean b);
+    public int getx();
+    public int gety();
 }
