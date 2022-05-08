ac2a95ac020024087b37986bc66385c45531d5c6
Ian Oliveira <93954736+IanOliveira718@users.noreply.github.com>
2022-05-08 18:55:52 -0300

    Update Figure.java

--- Projeto/Cod./figures/Figure.java
+++ Projeto/Cod./figures/Figure.java
@@ -14,6 +14,21 @@ public abstract class Figure{
    return(this.x<=x && x<=this.x+this.w && this.y<=y && y<=this.y+this.h);
    }
    
+   public boolean clickdrag(int x, int y){
+      return(x<=this.x+this.w && x>=this.x+this.w-7 && y<= this.y+ this.h && y>= this.y+ this.h-7);
+   }
+   public int getx(){
+      return (this.x);
+   }
+    
+   public int gety(){
+      return (this.y);
+   }
+
+   public void setf(boolean b){
+      this.foco=b;
+   }
+   
    public abstract void psize(int h, int w);
    public abstract void changeC(int g, int r, int b);
    public abstract void changeCi(int g, int r, int b);
