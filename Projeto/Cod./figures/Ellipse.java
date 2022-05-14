6263e85a473714bc93b978ee72e385943a69f9b1
Ian Oliveira <93954736+IanOliveira718@users.noreply.github.com>
2022-05-14 20:33:49 -0300

    Update Ellipse.java

--- Projeto/Cod./figures/Ellipse.java
+++ Projeto/Cod./figures/Ellipse.java
@@ -2,58 +2,82 @@ package figures;
 
 import java.awt.*;
 import java.awt.geom.Ellipse2D;
-import java.awt.geom.Ellipse2D.Double;
 
-public class Ellipse extends Figure{
-    private int ir,ig,ib;
+public class Ellipse extends Figure {
+    private int ir, ig, ib;
 
-    public Ellipse (int x, int y, int w, int h, int r, int g, int b, int ir, int ig, int ib, boolean foco)  {
+    public Ellipse(int x, int y, int w, int h, int r, int g, int b, int ir, int ig, int ib,boolean foco) {
         this.x = x;
         this.y = y;
         this.w = w;
         this.h = h;
-	this.r = r;
+        this.r = r;
         this.g = g;
         this.b = b;
-	this.ir = ir;
+        this.ir = ir;
         this.ig = ig;
         this.ib = ib;
-	this.foco=foco;
+        this.foco=foco;
     }
+    
+    public boolean clicked(int x, int y){
+        Ellipse2D.Double e = new Ellipse2D.Double(this.x, this.y, this.w, this.h);
+        return(e.contains(x,y));
+     }
 
-    public void paint (Graphics g) {
-
-	   Graphics2D g2d = (Graphics2D) g;
-	   g.setColor(new Color(this.r,this.g,this.b));
-           g2d.draw(new Ellipse2D.Double(this.x,this.y, this.w,this.h));
-	   g.setColor(new Color(this.ir,this.ig,this.ib));
-	   g2d.fill(new Ellipse2D.Double(this.x,this.y, this.w,this.h));
-	   if(foco){
-            	g.setColor(new Color(0, 0, 0));
-            	g.drawRect(this.x-1, this.y-1, this.w+2, this.h+2);
-		g.setColor(new Color(0, 0, 0));
-            	g.fillRect(this.x+this.w, this.y+ this.h, -7, -7);
+    public void paint(Graphics g,boolean b) {
+        this.foco=b;
+        Graphics2D g2d = (Graphics2D) g;
+        g.setColor(new Color(this.r, this.g, this.b));
+        g2d.draw(new Ellipse2D.Double(this.x, this.y, this.w, this.h));
+        g.setColor(new Color(this.ir, this.ig, this.ib));
+        g2d.fill(new Ellipse2D.Double(this.x, this.y, this.w, this.h));
+        if(foco){
+            g.setColor(new Color(0, 0, 0));
+            g.drawRect(this.x-1, this.y-1, this.w+2, this.h+2);
+            g.setColor(new Color(0, 0, 0));
+            g.fillRect(this.x+this.w, this.y+this.h, -7, -7);
         }
     }
-public void local(){
-	System.out.format("Local %d %d %d %d",this.x,this.y,this.w,this.h);
-	}
-	public void drag(int x,int y){
-		this.x=this.x+x;
-		this.y=this.y+y;
-	}
-	public  void mdrag(int x, int y){
+    public boolean clickdrag(int x, int y){
+        return(x<=this.x+this.w && x>=this.x+this.w-7 && y<= this.y+ this.h && y>= this.y+ this.h-7);
+     }
+     public int getx(){
+        return (this.x);
+     }
+      
+     public int gety(){
+        return (this.y);
+     }
+  
+     public void setf(boolean b){
+        this.foco=b;
+     }
+
+    public void local() {
+        System.out.format("Local %d %d %d %d", this.x, this.y, this.w, this.h);
+    }
+
+    public void drag(int x, int y) {
+        this.x = this.x + x;
+        this.y = this.y + y;
+    }
+
+    public  void mdrag(int x, int y){
         this.x=x-(this.w)/2;
         this.y=y-(this.h)/2;
     }
-    
-	
-     public void psize(int w, int h){
+    public boolean VerP(int x,int y, Figure f){
+        if (x >= f.x && x <= f.x + f.w && y >= f.y && y <= f.y + f.h){
+            return true;
+        }
+        return false;
+	}
+    public void psize(int w, int h){
         this.h = h;
         this.w = w;
      }
-     
-    public void changeC(int r, int g, int b){
+     public void changeC(int r, int g, int b){
         if(this.r+r<255){ 
             if(this.r+r<0){ this.r=0;}
             else{this.r=this.r+r;}
@@ -95,5 +119,5 @@ public void local(){
         }
         else{this.ib=255;}
     }
-	
+
 }
