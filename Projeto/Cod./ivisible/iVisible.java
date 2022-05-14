386c7409b7bfd755a4343d66b9167162310456cb
Ian Oliveira <93954736+IanOliveira718@users.noreply.github.com>
2022-05-14 20:31:00 -0300

    Update iVisible.java

--- Projeto/Cod./ivisible/iVisible.java
+++ Projeto/Cod./ivisible/iVisible.java
@@ -4,11 +4,9 @@ import java.awt.Graphics;
 
 public interface iVisible{
 
-    public boolean clicked(int x, int y);
-    public void paint (Graphics g);
     
-    public boolean clickdrag(int x, int y);
-    public void setf(boolean b);
-    public int getx();
-    public int gety();
+    public boolean clicked(int x, int y);
+
+    public void paint (Graphics g,boolean b);
+
 }
